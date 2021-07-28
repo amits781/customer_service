8f74637208d4c83dd3a6f0fa7c3d8deb744fe641
amits781@gmail.com <am113920>
2021-07-28 17:47:36 +0530

    Added model classes to producer service

---
+++ customer_service_producer/src/main/java/com/example/demo/CustomerServiceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceProducerApplication.class, args);
+	}
+
+}
