946472234b318f3de968a00a8baccfe8697f60b4
amits781@gmail.com <am113920>
2021-07-30 11:14:12 +0530

    Updated package names

--- customer_service_producer/src/main/java/com/producer/customer/controller/ProducerController.java
+++ customer_service_producer/src/main/java/com/producer/customer/controller/ProducerController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.producer.customer.controller;
 
 import javax.validation.Valid;
 import org.slf4j.Logger;
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.model.Customer;
+
+import com.producer.customer.model.Customer;
 
 @RestController
 @RequestMapping("/kafka")
--- customer_service_producer/src/main/java/com/producer/customer/model/Address.java
+++ customer_service_producer/src/main/java/com/producer/customer/model/Address.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+package com.producer.customer.model;
 
 import java.util.Objects;
 import javax.validation.constraints.*;
